834a8a28a57cd47dfa12ca860f8f2c36cbc032cf
= <=>
2022-03-19 17:06:04 +0000

    name

--- pages/api/regenerate.js
+++ pages/api/regenerate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REGENERATE_SECRET) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
