2e1ad81530ce5639cdf100f9dda3d0c62cf603b1
= <=>
2022-03-19 17:11:55 +0000

    send body back for debug

--- pages/api/regenerate.js
+++ pages/api/regenerate.js
@@ -1,13 +1,11 @@
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-    return res
-      .status(401)
-      .json({
-        message: 'Invalid token',
-        token: process.env.REVALIDATE_SECRET,
-        you_sent: req.query,
-      })
+    return res.status(401).json({
+      message: 'Invalid token',
+      token: process.env.REVALIDATE_SECRET,
+      you_sent: req.body,
+    })
   }
 
   try {
