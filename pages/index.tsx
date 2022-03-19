095be298465a4994e7a240c6e8c490850a53816e
= <=>
2022-03-19 15:20:37 +0000

    ISR dynamic posts page

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home({ posts }: Props) {
       {/* Posts */}
 
       <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
-        {[...posts, ...posts, ...posts].map((post) => (
+        {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="group cursor-pointer overflow-hidden rounded-lg border">
               <img
